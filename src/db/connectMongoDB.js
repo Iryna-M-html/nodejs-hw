e0af4630d72fd4fca69d88eafc59a35f70cb512f
Iryna-M-html <ivmohnataya@gmail.com>
2025-11-05 12:52:16 +0200

    mesagge connect mongodb

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export const connectMongoDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
-    console.log('Connected to MongoDB');
+    console.log('✅ MongoDB connection established successfully');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
   }
