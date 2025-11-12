78cfd8a7da7a2763b8d5abe3ef6f21d8abfe0bf6
Iryna-M-html <ivmohnataya@gmail.com>
2025-11-12 10:55:45 +0200

    branch  03-validation

--- src/server.js
+++ src/server.js
@@ -1,53 +1,35 @@
 // src/server.js
 import express from 'express';
 import cors from 'cors';
-import pino from 'pino-http';
+
 import 'dotenv/config';
+import { connectMongoDB } from './db/connectMongoDB.js';
+
+import helmet from 'helmet';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import notesRoutes from './routes/notesRoutes.js';
+import { logger } from './middleware/logger.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
-// Middleware
 app.use(express.json());
 app.use(cors());
-app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat:
-          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
-  }),
-);
+app.use(helmet());
+app.use(logger);
 
-// Решта коду
-
-// Middleware для парсингу JSON
-app.use(express.json());
-
-app.post('/users', (req, res) => {
-  console.log(req.body); // тепер тіло доступне як JS-об’єкт
-  res.status(201).json({ message: 'User created' });
+app.get('/', (req, res) => {
+  res.send('✅ API is running!');
 });
+app.use(notesRoutes);
+
+app.use(notFoundHandler);
+
+app.use(errorHandler);
+
+await connectMongoDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-});
-app.use((err, req, res, next) => {
-  console.error(err);
-
-  const isProd = process.env.NODE_ENV === 'production';
-
-  res.status(500).json({
-    message: isProd
-      ? 'Something went wrong. Please try again later.'
-      : err.message,
-  });
 });
