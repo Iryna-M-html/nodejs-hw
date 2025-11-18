c90a9f75fd29418b9557eae6c3ad82d8fad8e45e
Iryna-M-html <ivmohnataya@gmail.com>
2025-11-18 13:46:49 +0200

    Session

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,6 +5,7 @@ import { celebrate } from 'celebrate';
 import {
   loginUser,
   logoutUser,
+  refreshUserSession,
   registerUser,
 } from '../controllers/authController.js';
 import {
@@ -17,4 +18,5 @@ const router = Router();
 router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 router.post('/auth/logout', logoutUser);
+router.post('/auth/refresh', refreshUserSession);
 export default router;
