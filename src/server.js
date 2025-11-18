c3c33e7c9f181a89f925b56e829865af48c15184
Iryna-M-html <ivmohnataya@gmail.com>
2025-11-18 12:58:01 +0200

    auth/register

--- src/server.js
+++ src/server.js
@@ -6,29 +6,29 @@ import { errors } from 'celebrate';
 import 'dotenv/config';
 import { connectMongoDB } from './db/connectMongoDB.js';
 
-import helmet from 'helmet';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 import { logger } from './middleware/logger.js';
+import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 app.use(express.json());
 app.use(cors());
-app.use(helmet());
+
 app.use(logger);
 
 app.get('/', (req, res) => {
   res.send('✅ API is running!');
 });
+app.use(authRoutes);
 app.use(notesRoutes);
 
 app.use(notFoundHandler);
-
-app.use(errorHandler);
 app.use(errors());
+app.use(errorHandler);
 
 await connectMongoDB();
 
