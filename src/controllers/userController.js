e431d7b6d7682c0458de5d8bdb9b01c23cfc9239
Iryna-M-html <ivmohnataya@gmail.com>
2025-11-24 20:46:47 +0200

    add png avatar

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,10 @@
+import createHttpError from 'http-errors';
+// import { User } from '../models/user.js';
 export const updateUserAvatar = async (req, res, next) => {
+  if (!req.file) {
+    next(createHttpError(400, 'No file'));
+    return;
+  }
+  console.log(req.file);
   res.status(200).json({ url: '' });
 };
